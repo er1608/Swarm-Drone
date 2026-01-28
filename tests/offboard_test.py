51cf7a170b41fea315e09c4f8c781ad509ab5821
er1608 <khoatran16082003@gmail.com>
2026-01-28 09:57:58 +0700

    Update unittest

---
+++ tests/offboard_test.py
@@ -0,0 +1,21 @@
+import os
+import asyncio
+import pytest
+from unittest.mock import AsyncMock
+from mavsdk import PositionNedYaw
+from mavsdk import System
+
+@pytest.mark.asyncio
+async def test_start_offboard_mode():
+
+    mock_drone = AsyncMock(spec=System)
+    
+    mock_offboard = AsyncMock(spec=Offboard)
+    mock_drone.offboard = mock_offboard
+
+    mock_offboard.start.return_value = asyncio.Future()
+    mock_offboard.start.return_value.set_result(None)
+    
+    await mock_drone.offboard.start()
+    
+    mock_offboard.start.assert_awaited_once()
